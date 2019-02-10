aeb50062ac35022944a85234c1659bce2c2217c7
12q1 <thomasham89@gmail.com>
2019-02-10 22:13:12 +0100

    controlled form implemented

--- src/components/SearchUser.js
+++ src/components/SearchUser.js
@@ -1,16 +1,25 @@
 import React, {Component} from 'react';
 
 export default class SearchUser extends Component{
+    state = {name:''} //storing name in local state 
+
     handleSubmit(event){
         event.preventDefault()
-        console.log('Submitting form...')
+        console.log(`Submitting form with the name ${this.state.name}`) 
     }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]:event.target.value
+        })
+    }
+
     render(){
         return (
             <form onSubmit={this.handleSubmit}>
                 <label>
-                    Username:
-                    <input type="text" name="name"/>
+                    Github Username:
+                    <input type="text" name="name" onChange={this.handleChange} value={this.state.name}/>
                 </label>
                 <input type="submit" value="Search"/>
             </form>
