cc38833097951b67c1e4690b22d28a4300c314b9
12q1 <thomasham89@gmail.com>
2019-02-13 15:19:41 +0100

    added some css styling tweaks

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -7,14 +7,14 @@ export default function UserDetails(props) {
     return (
         <div className="Contacts">
             <h3>{user.login}</h3>
-            <img src={user.avatar_url} alt="user avatar" height="200"></img>
+            <img className="profilepic" src={user.avatar_url} alt="user avatar" height="200"></img>
             <p>Email: {user.email || "Not available"}</p>
             <p>Name: {user.name || "Not available"}</p>
             <a href={user.html_url}>Profile</a><br />
             <a href={`https://github.com/${user.login}?tab=repositories`}>Repository</a><br />
             <a href={`https://gist.github.com/${user.login}`}>Gists</a>
             <div className="repos">
-            <p>{user.login}'s most recent repos</p>
+            <p className="repotitle">{user.login}'s most recent projects</p>
             <ul>
             {repos.map(repo => <li key={repo.id}><a href={`https://github.com/${repo.full_name}`}>{repo.name}</a><br/>
             <p>{repo.description}</p></li>)}
--- src/components/UserList.js
+++ src/components/UserList.js
@@ -3,7 +3,7 @@ import * as React from 'react'
 export default function UserList(props) {
     return (
         <div>
-            <h3>Search Results</h3>
+            <h3>Search Results:</h3>
             <ul className="userlist">
                 {props.users.map(user =>
                     <li key={user.id} onClick={() => props.selectUser(user.id)}>
